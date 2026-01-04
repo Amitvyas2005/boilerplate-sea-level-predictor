06fc9d9ec95aef324f2acea1ba73ec9d85c04e0a
@Amit Vyas <amitbvyashji2005@gmail.com>
2026-01-04 23:48:40 +0530

    Fix: Correct year ranges for trend lines to match test expectations

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -10,20 +10,20 @@ def draw_plot():
     fig, ax = plt.subplots(figsize=(10, 6))
     
     # Create scatter plot
-    ax.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], alpha=0.7)
+    ax.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
     
-    # Create first line of best fit using all data
+    # Create first line of best fit using all data (1880-2050)
     slope1, intercept1, r_value1, p_value1, std_err1 = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    years_extended = list(df['Year']) + list(range(2014, 2051))
-    line1_y = [slope1 * year + intercept1 for year in years_extended]
-    ax.plot(years_extended, line1_y, color='red', label='Best fit (1880-2013)')
+    years1 = list(range(1880, 2051))
+    line1_y = [slope1 * year + intercept1 for year in years1]
+    ax.plot(years1, line1_y, 'r')
     
-    # Create second line of best fit using data from 2000 onwards
+    # Create second line of best fit using data from 2000 onwards (2000-2050)
     df_2000 = df[df['Year'] >= 2000]
     slope2, intercept2, r_value2, p_value2, std_err2 = linregress(df_2000['Year'], df_2000['CSIRO Adjusted Sea Level'])
-    years_2000_to_2050 = list(df_2000['Year']) + list(range(df_2000['Year'].max() + 1, 2051))
-    line2_y = [slope2 * year + intercept2 for year in years_2000_to_2050]
-    ax.plot(years_2000_to_2050, line2_y, color='green', label='Best fit (2000-2050)')
+    years2 = list(range(2000, 2051))
+    line2_y = [slope2 * year + intercept2 for year in years2]
+    ax.plot(years2, line2_y, 'g')
     
     # Add labels and title
     ax.set_xlabel('Year')
